d70580d79fee936eb9859de933739180e211ad07
mydevtestsetup <85756998+mydevtestsetup@users.noreply.github.com>
2021-08-02 22:21:09 +0530

    Create InternetConnectionExample.java

---
+++ InternetConnectionExample.java
@@ -0,0 +1,28 @@
+package javabeat.net.core;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class InternetConnectionExample {
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
+		try {
+			try {
+				URL url = new URL("http://www.google.com");
+				System.out.println(url.getHost());
+				HttpURLConnection con = (HttpURLConnection) url
+						.openConnection();
+				con.connect();
+				if (con.getResponseCode() == 200){
+					System.out.println("Connection established!!");
+				}
+			} catch (Exception exception) {
+				System.out.println("No Connection");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
